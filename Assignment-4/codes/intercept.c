33e72000443f641e9841e5b698f69bcd19e4c01f
Koushik11044 <muthyalaa.koushik@gmail.com>
2024-10-06 12:24:05 +0530

    Delete Assignment-4 directory

--- Assignment-4/codes/intercept.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void find_x_intercept(float n[], float c, float *x_intercept, float *y_intercept) {
-    *x_intercept = c / n[0];  // n[0] is the slope (m)
-    *y_intercept = 0;           // The y-coordinate is always 0 on the x-axis
-}
